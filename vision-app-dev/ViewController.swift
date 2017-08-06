a280c8d0818967c79bff456c86afbf27594e87e0
Xavier Maximin <xavier.maximin.uk@gmail.com>
2017-08-06 09:57:04 +0100

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by Xavier Maximin on 06/08/2017.
+//  Copyright © 2017 Xavier Maximin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
